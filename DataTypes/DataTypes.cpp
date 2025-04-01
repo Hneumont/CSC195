28451337bafd6e14d91d099ea16af4fc0ff741fe
Hneumont <hberumengovea@student.neumont.edu>
2025-04-01 16:09:52 -0600

    Datatype project started

---
+++ DataTypes/DataTypes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
